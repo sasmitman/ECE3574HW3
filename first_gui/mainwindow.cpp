9dc55771f78b93ca3b10951e4f307855d07cc24c
Xamists <sasmitman@gmail.com>
2014-03-26 14:46:14 -0400

    got example code to work

--- first_gui/mainwindow.cpp
+++ first_gui/mainwindow.cpp
@@ -1,14 +1,27 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
-}
+#include <QPushButton>
+#include <QGridLayout>
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+MainWindow::MainWindow()
+{
+ //central widget stores all other widgets
+ QWidget *myCentralWidget = new QWidget();
+ setCentralWidget (myCentralWidget);
+
+ // When myLayout is put under myCentralWidget, it automatically becomes its child.
+ QVBoxLayout *myLayout = new QVBoxLayout;
+ myCentralWidget->setLayout (myLayout);
+
+ QPushButton* closeBtn = new QPushButton("Close");
+ myLayout->addWidget(closeBtn);
+
+ QPushButton* dialogBtn = new QPushButton("Open Dialog Box!");
+ myLayout->addWidget(dialogBtn);
+}
+
